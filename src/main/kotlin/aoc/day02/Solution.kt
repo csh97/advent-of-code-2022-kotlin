396fd9f815f1baccc420944e887a5be9effe69b5
Conor Haworth <conor.haworth@awaze.co.uk>
2022-12-02 14:43:58 +0000

    Got rid of all the unsafe null calls :)

--- src/main/kotlin/aoc/day02/Solution.kt
+++ src/main/kotlin/aoc/day02/Solution.kt
@@ -17,7 +17,7 @@ fun main() {
             "Z" -> { partOneScore += 3 }
         }
 
-        val move = Move.valueOf(it.first)!!
+        val move = Move.valueOf(it.first)
         if (it.second == move.winningMove) {
             partOneScore += 6
         } else if (it.second == move.matching) {
@@ -29,18 +29,18 @@ fun main() {
     rounds.forEach {
         when (it.second) {
             "X" -> {
-                val winningMove = Move.valueOf(it.first)?.winningMoveActual
+                val winningMove = Move.valueOf(it.first).winningMoveActual
                 val moveSet = mutableListOf("A", "B", "C")
                 moveSet.removeAll(setOf(it.first, winningMove))
-                partTwoScore += Move.valueOf(moveSet.first())!!.points
+                partTwoScore += Move.valueOf(moveSet.first()).points
             }
             "Y" -> {
-                partTwoScore += Move.valueOf(it.first)!!.points
+                partTwoScore += Move.valueOf(it.first).points
                 partTwoScore += 3
             }
             "Z" -> {
-                val winningMove = Move.valueOf(it.first)?.winningMoveActual
-                partTwoScore += Move.valueOf(winningMove!!)!!.points
+                val winningMove = Move.valueOf(it.first).winningMoveActual
+                partTwoScore += Move.valueOf(winningMove).points
                 partTwoScore += 6
             }
         }
@@ -49,8 +49,6 @@ fun main() {
     println(partOneScore) //11386
     println(partTwoScore) //13600
 }
-
-
 
 enum class Move(val winningMoveActual: String, val points: Int, val winningMove: String, val matching: String) {
     A("B", 1, "Y", "X"),
